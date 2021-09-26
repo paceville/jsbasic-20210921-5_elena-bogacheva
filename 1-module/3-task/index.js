3cd5bfbb6ce8dafe9b373424a046034fb59a0c91
Elena <52384305+paceville@users.noreply.github.com>
2021-09-26 19:02:14 +0300

    module#1 task#3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,7 +2,6 @@ function ucFirst(str) {
   let strArr = str.split('');
 
   if (strArr.every(element => element != '')) {
-   // return str.firstLetterCaps();
     return str.charAt(0).toUpperCase() + str.slice(1)
   } else {
     return '';
